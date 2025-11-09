f3f803ef7a8fb56b565bfefa21ae026c0b89a692
Dennis K <denniskarnowitsch@gmail.com>
2025-11-09 04:39:08 +0100

    Check for .env on startup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,11 @@
+import { existsSync } from "fs";
 import type { Handle } from "@sveltejs/kit";
+
+// Check .env file exists on startup
+if (!existsSync(".env")) {
+  console.error("Missing .env file. Please create .env file with required configuration.");
+  process.exit(1);
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
   const session = event.cookies.get("session");
