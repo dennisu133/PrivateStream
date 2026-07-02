e9572c889f66de3d77a2fcd67e691084ce64c99e
Dennis K <denniskarnowitsch@gmail.com>
2026-07-02 15:03:33 +0200

    Remove csrf-origins.js

--- csrf/csrf-origins.js
+++
@@ -1,4 +0,0 @@
-// This file is for CSRF trusted origins.
-// It is imported by svelte.config.js and should be added to .gitignore
-// to keep your production domains private.
-export const trustedOrigins = ["http://192.168.1.104:3000", "https://kino.dennisu.com"];
